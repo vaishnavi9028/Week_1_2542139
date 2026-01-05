c034c75de437a3ee04893646582e854a757fcb37
vaishnavi9028 <vaishnaviutage77@gmail.com>
2026-01-05 14:24:48 +0530

    One.py

---
+++ myproject/One.py
@@ -0,0 +1,14 @@
+def cir_area():
+	r=3
+	print("area of circle is:",3.14*r*r)
+def rect_area():
+	l=10
+	b=20
+	print("area of rectangle is:",l*b)
+def tri_area():
+	b=5
+	h=6
+	print("area of triangle:",0.5*b*h)
+def sq_area():
+	side=2
+	print("area of square:",side*side)
